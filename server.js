26269ae9d27c26a378becc33fa5dfe8d5ae6b62d
Matt Kokolus <mjkokolus@gmail.com>
2024-02-15 20:25:08 -0500

    Started integration with Google Gemini

---
+++ server.js
@@ -0,0 +1,77 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai express
+
+const express = require('express');
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const dotenv = require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+const MODEL_NAME = "gemini-pro";
+const API_KEY = "AIzaSyDp4eUY70Zt7zUoP_2vnA77zMYNCUAUHio";
+
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    // ... other safety settings
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Your name is \"Pal\". You are a friendly, interview chatbot where you act as the interviewer I will act as a candidate. When I start a chat, I want you to ask me for the job I am applying to as well as the job description.After you get that information, I want you to conduct a mock interview for that job. I want you to be as realistic as possible and tailor questions towards that job. Following the interview questions, I want you to give me advice on my previous responses."}],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hello, thank you for interviewing with me today. My name is Pal and I will be your interviewer. Before we begin, what is the job role and the job description?"}],
+      },
+    ],
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/loader.gif', (req, res) => {
+  res.sendFile(__dirname + '/loader.gif');
+});
+app.post('/chat', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /chat req', userInput)
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const response = await runChat(userInput);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
